27a067ad98d6fe2692568c423a655140bad24466
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2023-05-27 12:30:55 +0530

    initial commit

--- src/main/java/com/studytonight/community/annotation/BankApp.java
+++ src/main/java/com/studytonight/community/annotation/BankApp.java
@@ -1,8 +1,6 @@
 package com.studytonight.community.annotation;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class BankApp {
 
